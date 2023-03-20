f0a40e8327304a59e9e0f62e09376b8331ff8939
Evan Cameron <w0200257@campus.nscc.ca>
2023-03-19 22:27:40 -0300

    Ch03 01

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet,
+  Text, 
+  View, 
+  Button, 
+  ActivityIndicator, 
+  ProgressViewIOS, 
+  Alert, 
+  Dimensions,
+  Platform,
+  ProgressBarAndroid
+ } from 'react-native';
+
+const { height, width } = Dimensions.get('window');
 
 export default function App() {
+  const onButtonPress = () => {
+    Alert.alert(`${new Date().toLocaleDateString()} button press`)
+  }
   return (
-    <View style={styles.container}>
-      <Text>Ready...</Text>
-      <StatusBar style="auto" />
+    <View style={{ padding: 50 }}>
+      {Platform.OS === "ios" && <ProgressViewIOS progress={0.5} />}
+      {Platform.OS === "android" && (
+        <ProgressViewIOS 
+          styleAttr="Horizontal"
+          indeterminate={false}
+          color="blue"
+          progress={0.5} 
+        />
+      )}
+      <ActivityIndicator size="large" color="#61DBFB" />
+      <Button title="click me" onPress={onButtonPress} />
+      <Text>Height: {height} </Text>
+      <Text> Width: {width} </Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
