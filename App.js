26c579f9871424c263da1136b69591cc56795715
Evan Cameron <w0200257@campus.nscc.ca>
2023-03-20 15:50:45 -0300

    ch04 03 extracting a custom component

--- App.js
+++ App.js
@@ -6,19 +6,29 @@ import {
   View }
 from 'react-native';
 
+function ColorButton({ backgroundColor, onPress=f=>f }) {
+  return (
+    <TouchableHighlight style={styles.button}
+        onPress={() => onPress(backgroundColor)}
+        underlayColor='orange'
+      >
+        <View style={styles.row}>
+          <View style={[styles.sample, {backgroundColor}]} />
+          <Text style={styles.buttonText}>{backgroundColor}</Text>
+        </View>
+      </TouchableHighlight>
+  )
+}
+
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState("blue");
   return (
     <View style={[styles.container, {backgroundColor}]}>
-      <TouchableHighlight style={styles.button}
-        onPress={() => setBackgroundColor('yellow')}
-        underlayColor='orange'
-      >
-        <View style={styles.row}>
-          <View style={[styles.sample, {backgroundColor: "yellow"}]} />
-          <Text style={styles.buttonText}>yellow</Text>
-        </View>
-      </TouchableHighlight>
+      <ColorButton backgroundColor='red' onPress={setBackgroundColor}/>
+      <ColorButton backgroundColor='green' onPress={setBackgroundColor} />
+      <ColorButton backgroundColor='blue' onPress={setBackgroundColor} />
+      <ColorButton backgroundColor='yellow' onPress={setBackgroundColor}/>
+      <ColorButton backgroundColor='purple' onPress={setBackgroundColor}/>
     </View>
   );
 }
@@ -36,7 +46,7 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderRadius: 10, 
     alignSelf: 'stretch',
-    backgroundColor: 'rgba(255,255,255, .8'
+    backgroundColor: 'rgba(255,255,255, .8)'
   },
   buttonText: {
     fontSize: 30,
