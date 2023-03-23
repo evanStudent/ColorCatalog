1b7ceaa7f6fbd9c241cf7b76aa80dfc397fe0097
Evan Cameron <w0200257@campus.nscc.ca>
2023-03-23 18:24:17 -0300

    ch05 06 using refresh control

--- App.js
+++ App.js
@@ -1,27 +1,33 @@
 import * as React from "react";
-import { Text, SafeAreaView, ScrollView, StyleSheet } from 'react-native';
+import { Text, SafeAreaView, ScrollView, StyleSheet, ActivityIndicator, RefreshControl } from 'react-native';
 import Constants from 'expo-constants';
 
 
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
   const [pet, setPet] = useState();
 
   const loadPet = async () => {
+    setLoading(true);
     const res = await fetch("http://pet-library.moonhighway.com/api/randomPet")
     const data = await res.json();
+    await Image.prefetch(data.photo.full)
     setPet(data);
+    setLoading(false);
   }
 
   useEffect(() => {
     loadPet();
   }, []);
 
-  if (!pet) return null;
+  if (!pet) return <ActivityIndicator size="large" />;
 
   return (
     <SafeAreaView>
-      <ScrollView>
+      <ScrollView
+        refreshControl={<RefreshControl refreshing={loading} onRefresh={loadPet} />
+        }>
         <Image style={styles.pic} source={{uri: pet.photo.full }} />
         <Text style={styles.paragraph}>{pet.name} - {pet.category}</Text>
       </ScrollView>
