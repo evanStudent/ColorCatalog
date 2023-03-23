427c10c55ba7ff3a1bc5729ff4afdfb54a1363cc
Evan Cameron <w0200257@campus.nscc.ca>
2023-03-23 18:03:17 -0300

    ch05-03 navigating between screens

--- hooks.js
+++ hooks.js
@@ -1,27 +1,30 @@
 import { useState, useEffect } from "react";
 import { generate } from "shortid";
-import { AsyncStorage } from "react-native"
+import { AsyncStorage } from "react-native";
 
 export const useColors = () => {
   const [colors, setColors] = useState([]);
 
   const loadColors = async () => {
-    const colorData = await AsyncStorage.getItem("@ColorListStore:Colors");
+    const colorData = await AsyncStorage.getItem(
+      "@ColorListStore:Colors"
+    );
     if (colorData) {
       const colors = JSON.parse(colorData);
       setColors(colors);
     }
-  }
+  };
 
-  // Load Colors
   useEffect(() => {
     if (colors.length) return;
     loadColors();
   }, []);
 
-  // Save Colors
   useEffect(() => {
-    AsyncStorage.setItem("@ColorListStore:Colors", JSON.stringify(colors));
+    AsyncStorage.setItem(
+      "@ColorListStore:Colors",
+      JSON.stringify(colors)
+    );
   }, [colors]);
 
   const addColor = color => {
